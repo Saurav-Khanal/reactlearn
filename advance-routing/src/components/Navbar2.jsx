72591eb19e394aedaf686f5623e83991b816c36c
Sauravc-Dev <SAURAVKHANAL842@GMAIL.COM>
2026-08-12 14:34:13 +0545

    how to send the data from child to parent using use state

--- advance-routing/src/components/Navbar2.jsx
+++ advance-routing/src/components/Navbar2.jsx
@@ -20,7 +20,7 @@ const Navbar2 = () => {
       
       <button onClick={()=>{
         navigate(+1)
-      }} className='font-medium bg-emerald-800 px-5 py-2 rounded m-2 cursor-pointer active:scale-95' >
+      }} className='font-medium bg-emerald-800 px-5 py-2 rounded-xl m-2 cursor-pointer active:scale-95' >
         next
       </button>
 
